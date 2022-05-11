7a73032e30c19249a2f0dde1ff2d416ed25d7310
Arthur Sobreira <tutusobreira@gmail.com>
2022-05-11 16:41:57 -0300

    Refatoração no arquivo: 01_CompararValores

--- src/02_Tabuada.py
+++ src/02_Tabuada.py
@@ -1,13 +1,13 @@
 #Faça um programa para exibir a tabuada de 0 a 9.
 
-def mult_table(number):
+def multi_table(number):
     for c in range(10):
         print(f'{number} * {c} = {number * c}')
 
 
 def main():
     num = int(input('Input a number: '))
-    mult_table(num)
+    multi_table(num)
 
 
 if __name__ == '__main__':
